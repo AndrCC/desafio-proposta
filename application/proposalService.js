176baf02ac7e84b4f6aee8a71c0b44c17856af29
AndrCC <andre.cavalcantte@hotmail.com>
2024-02-15 11:31:21 -0300

    retirado comentários

--- application/proposalService.js
+++ application/proposalService.js
@@ -1,8 +1,7 @@
 const fs = require('fs').promises;
 const path = require('path');
-const Proposal = require('../domain/Proposal'); // Ajuste o caminho conforme necessário
+const Proposal = require('../domain/Proposal'); 
 
-// Função para validar e salvar propostas
 async function validateAndSaveProposals(propostas) {
     let validCount = 0;
     let invalidCount = 0;
@@ -10,20 +9,18 @@ async function validateAndSaveProposals(propostas) {
         const proposal = new Proposal(propostaData.propostaId, propostaData.produto, propostaData.dataInicioVigencia, propostaData.dataFimVigencia);
         if (proposal.isValid()) {
             validCount++;
-            return propostaData; // Retornar proposta válida para salvar
+            return propostaData; 
         } else {
             invalidCount++;
-            return null; // Proposta inválida, não salvar
+            return null; 
         }
-    }).filter(proposal => proposal !== null); // Filtrar propostas válidas
+    }).filter(proposal => proposal !== null); 
 
-    // Salvar propostas validadas em um arquivo
     await fs.writeFile(path.join(__dirname, '..', 'json', 'proposals.json'), JSON.stringify(validatedProposals, null, 2));
 
     return { valid: validCount, invalid: invalidCount };
 }
 
-// Função para recuperar o produto de uma proposta específica
 async function getProposalProduct(propostaId) {
     const data = await fs.readFile(path.join(__dirname, '..', 'json', 'proposals.json'), 'utf8');
     const propostas = JSON.parse(data);
--- domain/Proposal.js
+++ domain/Proposal.js
@@ -8,9 +8,8 @@ class Proposal {
 
     isValid() {
         const proposalString = String(this.propostaId);
-        // Assumindo que o nrVersaoOferta é corretamente passado como os 2 últimos dígitos do propostaId
         const nrVersaoOferta = this.produto.nrVersaoOferta;
-        const digits = proposalString.slice(0, 8).split('').map(Number); // Pega apenas os 8 primeiros dígitos
+        const digits = proposalString.slice(0, 8).split('').map(Number); 
         let sumEven = 0, sumOdd = 0;
 
         digits.forEach(digit => {
